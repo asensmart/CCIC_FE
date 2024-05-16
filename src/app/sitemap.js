207efe2f6978d1e25f523da4d6d4116e51ea3522
asensmart <asenali.g@dyooti.com>
2024-05-17 00:31:42 +0530

    dynamic sitemap has been added

---
+++ src/app/sitemap.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+export default async function sitemap() {
+  var mapData = [];
+  const areas = await axios.get(
+    `${process.env.NEXT_PUBLIC_API_URL}/get/sitemapData`
+  );
+
+  const defaultData = [
+    {
+      url: "https://customercareinchennai.com/",
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: "https://customercareinchennai.com/aboutus",
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: "https://customercareinchennai.com/contactus",
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+  ];
+
+  mapData = [...defaultData, ...areas?.data?.data];
+
+  return mapData;
+}
