03a94cca98ff1420f8d52f854c1ce6cfa5ed9198
asensmart <asenali.g@dyooti.com>
2024-07-07 18:33:10 +0530

    brand contact & about error solved

--- src/app/[brand]/aboutus/page.js
+++ src/app/[brand]/aboutus/page.js
@@ -1,13 +1,28 @@
-import AboutContent from '@/app/components/about';
-import React from 'react'
+import AboutContent from "@/app/components/about";
+import axios from "axios";
+import { notFound } from "next/navigation";
+import React from "react";
 
-const BrandAboutUs = ({params}) => {
-  console.log(params)
+const BrandAboutUs = async ({ params }) => {
+  const getBrand = await axios
+    .post(`${process.env.NEXT_PUBLIC_BASE_URL}/brand`, {
+      brandName: params?.brand,
+    })
+    .then((res) => {
+      return res?.data?.data;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  if (getBrand?.length == 0) notFound();
+
+  console.log(params);
   return (
     <div>
       <AboutContent name={params?.brand} isHome={true} />
     </div>
   );
-}
+};
 
-export default BrandAboutUs
+export default BrandAboutUs;
--- src/app/[brand]/contactus/page.js
+++ src/app/[brand]/contactus/page.js
@@ -1,12 +1,27 @@
-import ContactUsPage from '@/app/components/contact';
-import React from 'react'
+import ContactUsPage from "@/app/components/contact";
+import axios from "axios";
+import { notFound } from "next/navigation";
+import React from "react";
 
-const BrandContactUs = () => {
+const BrandContactUs = async ({ params }) => {
+  const getBrand = await axios
+    .post(`${process.env.NEXT_PUBLIC_BASE_URL}/brand`, {
+      brandName: params?.brand,
+    })
+    .then((res) => {
+      return res?.data?.data;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  if (getBrand?.length == 0) notFound();
+
   return (
     <div>
       <ContactUsPage />
     </div>
   );
-}
+};
 
-export default BrandContactUs
+export default BrandContactUs;
